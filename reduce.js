a2a59d0713433c62f827e16e67896f5e47957f39
Manas Sarma <manassarma121@gmail.com>
2021-10-07 15:36:03 +0530

    second commit

--- reduce.js
+++ reduce.js
@@ -2,14 +2,15 @@
 // and value represents each element
 // so retun the accumulator
 
-const vals = [10, 5, 25, 20, 7, 13]         
-
+const vals = [10, 5, 25, 20, 7, 13]
 //Reduce and calculate sum
-
-const sum = vals.reduce((acc,val) => acc + val);
+const sum = vals.reduce((acc, val) => acc + val);
 console.log('sum =', sum);
-
 //Max of array
-
-const largest = vals.reduce((acc,val) => acc > val ? acc : val);
+const largest = vals.reduce((acc, val) => acc > val ? acc : val);
 console.log("largest is", largest);
+
+
+const fruits = ['apple', 'banana', 'mango', 'orange']
+const fruitsReduce = fruits.map((fruit) => fruit);
+console.log(fruitsReduce);
